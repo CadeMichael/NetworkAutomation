74b8c6675051a0474b4559770071dedba8e2b9b1
CadeMichael <cade.lueker@gmail.com>
2026-03-23 23:03:02 -0600

    ssh command

--- ssh.sh
+++ ssh.sh
@@ -2,4 +2,4 @@
 ssh -oKexAlgorithms=+diffie-hellman-group14-sha1 \
                -oHostKeyAlgorithms=+ssh-rsa \
                -oCiphers=+aes128-cbc \
-               lab@198.51.100.12
+               -s cade@198.51.100.1 netconf
